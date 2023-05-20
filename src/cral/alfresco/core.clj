eb8f6376675e6a1c41027f722aececf8026c5c9a
saidone75 <saidone@saidone.org>
2023-05-20 16:31:56 +0200

    order records

--- src/cral/alfresco/core.clj
+++ src/cral/alfresco/core.clj
@@ -6,15 +6,6 @@
   (:import (clojure.lang PersistentHashMap PersistentVector)
            (java.io File)))
 
-(defn get-node
-  "Get node metadata."
-  [ticket node-id & [query-params]]
-  (utils/call-rest
-    client/get
-    (format "%s/nodes/%s" (config/get-url 'core) node-id)
-    ticket
-    {:query-params query-params}))
-
 (defrecord NodeBodyUpdate
   [^String name
    ^String node-type
@@ -29,6 +20,20 @@
 (defrecord Permissions
   [^Boolean is-inheritance-enabled
    ^PersistentVector locally-set])
+
+(defrecord NodeBodyCreate
+  [^String name
+   ^String node-type
+   ^PersistentHashMap properties])
+
+(defn get-node
+  "Get node metadata."
+  [ticket node-id & [query-params]]
+  (utils/call-rest
+    client/get
+    (format "%s/nodes/%s" (config/get-url 'core) node-id)
+    ticket
+    {:query-params query-params}))
 
 (defn update-node
   "Update a node."
@@ -56,11 +61,6 @@
     (format "%s/nodes/%s/children" (config/get-url 'core) node-id)
     ticket
     {:query-params query-params}))
-
-(defrecord NodeBodyCreate
-  [^String name
-   ^String node-type
-   ^PersistentHashMap properties])
 
 (defn create-node
   "Create a node."
