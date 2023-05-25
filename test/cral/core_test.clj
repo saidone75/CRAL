a8eef2fff4d790233d77e0168cf5fda446d01134
saidone75 <saidone@saidone.org>
2023-05-25 17:47:27 +0200

    copy-node test

--- test/cral/core_test.clj
+++ test/cral/core_test.clj
@@ -10,17 +10,20 @@
             [cral.alfresco.search :as search]
             [cral.alfresco.auth :as auth]))
 
+(def user "admin")
+(def pass "admin")
+
 (defn get-guest-home
   []
   (:entry (first
             (get-in
-              (let [ticket (model/map->Ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry]))
+              (let [ticket (model/map->Ticket (get-in (auth/create-ticket user pass) [:body :entry]))
                     search-request (search/map->SearchRequest {:query (search/map->RequestQuery {:query "PATH:'app:company_home/app:guest_home'"})})]
                 (search/search ticket search-request))
               [:body :list :entries]))))
 
 (deftest get-node
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         guest-home-id (:id (get-guest-home))]
     ;; well known fields for query parameters defined in core/QueryParamsGetNode
     (core/get-node ticket guest-home-id (model/map->GetNodeQueryParams {:include ["path" "permissions"]}))
@@ -28,7 +31,7 @@
     (core/get-node ticket guest-home-id {:include ["path" "permissions"]})))
 
 (deftest update-node
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         parent-id (:id (get-guest-home))
         node-body-create (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})
         node-id (get-in (core/create-node ticket parent-id node-body-create) [:body :entry :id])
@@ -39,14 +42,14 @@
     (core/delete-node ticket node-id)))
 
 (deftest delete-node
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         parent-id (:id (get-guest-home))
         node-body-create (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})
         node-id (get-in (core/create-node ticket parent-id node-body-create) [:body :entry :id])]
     (is (= 204 (:status (core/delete-node ticket node-id))))))
 
 (deftest list-node-children
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         company-home-id (get-in (first (get-in (search/search ticket (search/map->SearchRequest {:query (search/map->RequestQuery {:query "PATH:'app:company_home'"})})) [:body :list :entries])) [:entry :id])
         list-node-children-response (core/list-node-children ticket company-home-id)]
     (is (= 200 (:status list-node-children-response)))
@@ -54,7 +57,7 @@
     (is (not (nil? (some #(= "Sites" (:name %)) (map :entry (get-in list-node-children-response [:body :list :entries]))))))))
 
 (deftest create-node
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         parent-id (:id (get-guest-home))
         node-body-create (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})
         create-node-response (core/create-node ticket parent-id node-body-create)]
@@ -62,8 +65,20 @@
     ;; clean up
     (core/delete-node ticket (get-in create-node-response [:body :entry :id]))))
 
+(deftest copy-node
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
+        parent-id (:id (get-guest-home))
+        created-node-id (get-in (core/create-node ticket parent-id (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])
+        new-parent-id (get-in (core/create-node ticket parent-id (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:folder"})) [:body :entry :id])
+        copy-node-body (model/map->CopyNodeBody {:target-parent-id new-parent-id :name (.toString (UUID/randomUUID))})
+        copy-node-response (core/copy-node ticket created-node-id copy-node-body)]
+    (is (= (get-in copy-node-response [:body :entry :parent-id]) new-parent-id))
+    ;; clean up
+    (core/delete-node ticket created-node-id)
+    (core/delete-node ticket new-parent-id)))
+
 (deftest get-node-content
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         parent-id (:id (get-guest-home))
         node-body-create (model/map->CreateNodeBody {:name (str (.toString (UUID/randomUUID)) ".txt") :node-type "cm:content"})
         node-id (get-in (core/create-node ticket parent-id node-body-create) [:body :entry :id])
@@ -85,7 +100,7 @@
       (io/delete-file downloaded-file))))
 
 (deftest update-node-content
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         parent-id (:id (get-guest-home))
         node-body-create (model/map->CreateNodeBody {:name (str (.toString (UUID/randomUUID)) ".txt") :node-type "cm:content"})
         node-id (get-in (core/create-node ticket parent-id node-body-create) [:body :entry :id])
@@ -98,6 +113,6 @@
     (io/delete-file file-to-be-uploaded)))
 
 (deftest list-parents
-  (let [ticket (get-in (auth/create-ticket "admin" "admin") [:body :entry])
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         node-id (:id (get-guest-home))]
     (is (= "Company Home" (get-in (first (get-in (core/list-parents ticket node-id) [:body :list :entries])) [:entry :name])))))
