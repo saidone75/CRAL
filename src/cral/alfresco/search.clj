8a4b754958201f61c866f96d0d7af45eee822548
saidone <saidone@saidone.org>
2023-05-27 11:22:39 +0200

    opts for search

--- src/cral/alfresco/search.clj
+++ src/cral/alfresco/search.clj
@@ -3,7 +3,8 @@
             [clojure.data.json :as json]
             [cral.alfresco.config :as config]
             [cral.utils.utils :as utils])
-  (:import (cral.alfresco.model Ticket)))
+  (:import (clojure.lang PersistentHashMap)
+           (cral.alfresco.model Ticket)))
 
 (defrecord RequestQuery [^String language ^String user-query ^String query])
 (defrecord Paging [^Integer max-items ^Integer skip-count])
@@ -11,10 +12,11 @@
 
 (defn search
   "Searches Alfresco"
-  [^Ticket ticket ^SearchRequest search-request]
+  [^Ticket ticket ^SearchRequest search-request & [^PersistentHashMap opts]]
   (utils/call-rest
     client/post
     (format "%s/search" (config/get-url 'search))
     ticket
     {:body         (json/write-str (utils/camel-case-stringify-keys search-request))
-     :content-type :json}))
+     :content-type :json}
+    (:return-headers opts)))
