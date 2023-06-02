6cb1a14a6533e9a242a1d218813d01ede58ac4f8
saidone <saidone@saidone.org>
2023-06-02 16:54:14 +0200

    update license link

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A library for consuming Alfresco Content Services public REST API"
   :url "https://saidone.org"
   :license {:name "GNU General Public License v3.0"
-            :url  "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+            :url  "https://raw.githubusercontent.com/saidone75/CRAL/main/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
                  [org.clj-commons/clj-http-lite "1.0.13"]
