3a2d556e1a3d023f7da19bf1c2544f21817b9f3a
saidone <saidone@saidone.org>
2023-06-10 08:08:14 +0200

    create-tag test

--- test/cral/tags_tests.clj
+++ test/cral/tags_tests.clj
@@ -10,14 +10,20 @@
 (def user "admin")
 (def pass "admin")
 
-(deftest list-tags
+(deftest create-then-list-then-delete-node-tags
   (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         parent-id (:id (tu/get-guest-home ticket))
         ;; create a node
-        node-id (get-in (nodes/create-node ticket parent-id (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])]
+        node-id (get-in (nodes/create-node ticket parent-id (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])
+        tag-name (.toString (UUID/randomUUID))]
+    ;; create a tag for the node
+    (= 201 (:status (tags/create-node-tag ticket node-id [(model/map->CreateNodeTagBody {:tag tag-name})])))
     ;; list tags
     (let [response (tags/list-node-tags ticket node-id)]
-      (is (= 204 (:status (nodes/delete-node ticket node-id))))
-      response)))
+      (is (= 200 (:status response)))
+      (is (= tag-name (get-in (first (get-in response [:body :list :entries])) [:entry :tag]))) [:tag])
+    ;; FIXME delete tag
+    ;; clean up
+    (is (= 204 (:status (nodes/delete-node ticket node-id))))))
 
 
