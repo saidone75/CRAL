8e09bb05556958f38c13e3cb8aed3672d05fa5ee
saidone <saidone@saidone.org>
2023-06-10 18:01:32 +0200

    release

--- src/cral/alfresco/core/comments.clj
+++ src/cral/alfresco/core/comments.clj
@@ -2,7 +2,8 @@
   (:require [clj-http.lite.client :as client]
             [clojure.data.json :as json]
             [cral.alfresco.config :as config]
-            [cral.utils.utils :as utils])
+            [cral.utils.utils :as utils]
+            [cral.alfresco.model.core])
   (:import (clojure.lang PersistentHashMap PersistentVector)
            (cral.alfresco.model.auth Ticket)
            (cral.alfresco.model.core CreateCommentQueryParams
