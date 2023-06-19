c9e15fec1fb83ab39ff1ac8f7f97435a1a8f3a89
saidone <saidone@saidone.org>
2023-06-19 08:30:34 +0200

    list-groups-test

---
+++ test/cral/groups_test.clj
@@ -0,0 +1,13 @@
+(ns cral.groups-test
+  (:require [clojure.test :refer :all]
+            [cral.alfresco.auth :as auth]
+            [cral.alfresco.core.groups :as groups]
+            [cral.alfresco.model.core]))
+
+(def user "admin")
+(def pass "admin")
+
+(deftest list-groups
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
+        list-groups-response (groups/list-groups ticket)]
+    (is (= 200 (:status list-groups-response)))))
