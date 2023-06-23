0435abfd6fa4e3c5879485184a9c3e98bee17e5e
saidone <saidone@saidone.org>
2023-06-23 07:58:59 +0200

    more groups tests

--- test/cral/groups_test.clj
+++ test/cral/groups_test.clj
@@ -14,4 +14,14 @@
 (deftest list-groups
   (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         list-groups-response (groups/list-groups ticket)]
-    (is (= 200 (:status list-groups-response)))))
+    (is (= 200 (:status list-groups-response)))
+    (is (->> list-groups-response
+             (#(get-in % [:body :list :entries]))
+             (map #(get-in % [:entry :id]))
+             (some #(= "GROUP_ALFRESCO_ADMINISTRATORS" %))))))
+
+(deftest get-group-details
+  (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
+        get-group-details-response (groups/get-group-details ticket "GROUP_ALFRESCO_ADMINISTRATORS")]
+    (is (= 200 (:status get-group-details-response)))
+    (is (get-in get-group-details-response [:body :entry :is-root]))))
