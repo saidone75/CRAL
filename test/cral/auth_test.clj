be24fec816a6087792e993963bd5be454451bdbd
saidone75 <saidone@saidone.org>
2023-06-27 14:00:15 +0200

    user and password vars for auth test

--- test/cral/auth_test.clj
+++ test/cral/auth_test.clj
@@ -2,16 +2,19 @@
   (:require [clojure.test :refer :all]
             [cral.alfresco.auth :as auth]))
 
+(defonce user "admin")
+(defonce password "admin")
+
 (deftest create-ticket
-  (let [ticket-response (auth/create-ticket "admin" "admin")]
+  (let [ticket-response (auth/create-ticket user password)]
     (is (= 201 (:status ticket-response)))
     (is (= "admin" (get-in ticket-response [:body :entry :user-id])))
     (is (not (nil? (get-in ticket-response [:body :entry :id]))))))
 
 (deftest validate-ticket
-  (let [ticket-response (auth/create-ticket "admin" "admin")]
+  (let [ticket-response (auth/create-ticket user password)]
     (is (= 200 (:status (auth/validate-ticket (get-in ticket-response [:body :entry])))))))
 
 (deftest delete-ticket
-  (let [ticket-response (auth/create-ticket "admin" "admin")]
+  (let [ticket-response (auth/create-ticket user password)]
     (is (= 204 (:status (auth/delete-ticket (get-in ticket-response [:body :entry])))))))
