d609e49f69f468634cfd46457f40e86a762c6d35
saidone <saidone@saidone.org>
2023-07-18 07:49:35 +0200

    people docstrings

--- src/cral/alfresco/core/people.clj
+++ src/cral/alfresco/core/people.clj
@@ -13,7 +13,10 @@
                                      ListPeopleQueryParams UpdatePersonBody UpdatePersonQueryParams)))
 
 (defn create-person
-  "Create a person."
+  "Create a person.
+  If applicable, the given person's login access can also be optionally disabled.
+  You must have admin rights to create a person.
+  More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/people/createPerson)."
   ([^Ticket ticket ^CreatePersonBody body]
    (create-person ticket body nil))
   ([^Ticket ticket ^CreatePersonBody body ^CreatePersonQueryParams query-params & [^PersistentHashMap opts]]
@@ -27,7 +30,11 @@
      opts)))
 
 (defn list-people
-  "List people."
+  "List people.
+  You can use the **include** parameter to return any additional information.
+  The default sort order for the returned list is for people to be sorted by ascending id.
+  You can override the default by using the **order-by** parameter.
+  More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/people/listPeople)."
   ([^Ticket ticket]
    (list-people ticket nil))
   ([^Ticket ticket ^ListPeopleQueryParams query-params & [^PersistentHashMap opts]]
@@ -39,7 +46,9 @@
      opts)))
 
 (defn get-person
-  "Get a person."
+  "Gets information for the person **person-id**.
+  You can use the `-me-` string in place of `person-id` to specify the currently authenticated user.
+  More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/people/getPerson)."
   ([^Ticket ticket ^String person-id]
    (get-person ticket person-id nil))
   ([^Ticket ticket ^String person-id ^GetPersonQueryParams query-params & [^PersistentHashMap opts]]
@@ -51,7 +60,14 @@
      opts)))
 
 (defn update-person
-  "Update a person."
+  "Update the given person's details.
+  You can use the `-me-` string in place of `person-id` to specify the currently authenticated user.
+  If applicable, the given person's login access can also be optionally disabled or re-enabled.
+  You must have admin rights to update a person — unless updating your own details.
+  f you are changing your password, as a non-admin user, then the existing password must also be supplied (using the old-password field in addition to the new password value).
+  Admin users cannot be disabled by setting enabled to false.
+  Non-admin users may not disable themselves.
+  More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/people/updatePerson)."
   ([^Ticket ticket ^String person-id ^UpdatePersonBody body]
    (update-person ticket person-id body nil))
   ([^Ticket ticket ^String person-id ^UpdatePersonBody body ^UpdatePersonQueryParams query-params & [^PersistentHashMap opts]]
