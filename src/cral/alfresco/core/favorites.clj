fbf1503fac30127375b1b229b6a55e87b86f6078
saidone <saidone@saidone.org>
2024-03-27 07:39:46 +0100

    create-favorite

--- src/cral/alfresco/core/favorites.clj
+++ src/cral/alfresco/core/favorites.clj
@@ -1,12 +1,13 @@
 (ns cral.alfresco.core.favorites
   (:require [clj-http.lite.client :as client]
+            [clojure.data.json :as json]
             [cral.alfresco.config :as config]
+            [cral.utils.utils :as utils]
             [cral.alfresco.model.auth]
-            [cral.alfresco.model.core]
-            [cral.utils.utils :as utils])
-  (:import (clojure.lang PersistentHashMap)
+            [cral.alfresco.model.core])
+  (:import (clojure.lang PersistentHashMap PersistentVector)
            (cral.alfresco.model.auth Ticket)
-           (cral.alfresco.model.core ListFavoritesQueryParams)))
+           (cral.alfresco.model.core CreateFavoriteQueryParams ListFavoritesQueryParams)))
 
 (defn list-favorites
   "Gets a list of favorites for person **person-id**.
@@ -20,4 +21,20 @@
      (format "%s/people/%s/favorites" (config/get-url 'core) person-id)
      ticket
      {:query-params query-params}
+     opts)))
+
+(defn create-favorite
+  "Favorite a site, file, or folder in the repository.
+  You can use the -me- string in place of **personId** to specify the currently authenticated user.
+  More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/favorites/createFavorite)."
+  ([^Ticket ticket ^String person-id ^PersistentVector body]
+   (create-favorite ticket person-id body nil))
+  ([^Ticket ticket ^String person-id ^PersistentVector body ^CreateFavoriteQueryParams query-params & [^PersistentHashMap opts]]
+   (utils/call-rest
+     client/post
+     (format "%s/people/%s/favorites" (config/get-url 'core) person-id)
+     ticket
+     {:body         (json/write-str (utils/camel-case-stringify-keys body))
+      :query-params query-params
+      :content-type :json}
      opts)))
--- test/cral/favorites_test.clj
+++ test/cral/favorites_test.clj
@@ -2,7 +2,11 @@
   (:require [clojure.test :refer :all]
             [cral.alfresco.auth :as auth]
             [cral.alfresco.core.favorites :as favorites]
-            [cral.alfresco.model.core]))
+            [cral.alfresco.core.nodes :as nodes]
+            [cral.alfresco.model.core :as model]
+            [cral.alfresco.model.core]
+            [cral.test-utils :as tu])
+  (:import (java.util UUID)))
 
 (def user "admin")
 (def password "admin")
@@ -11,3 +15,19 @@
   (let [ticket (get-in (auth/create-ticket user password) [:body :entry])]
     ;; list favorites
     (is (= 200 (:status (favorites/list-favorites ticket user))))))
+
+(deftest create-then-list-favorites
+  (let [ticket (get-in (auth/create-ticket user password) [:body :entry])
+        parent-id (:id (tu/get-guest-home ticket))
+        ;; create a node
+        create-node-body (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})
+        node-id (get-in (nodes/create-node ticket parent-id create-node-body) [:body :entry :id])]
+    ;; add node to favorites
+    (favorites/create-favorite ticket "-me-" [(model/->CreateFavoriteBody {:file {:guid node-id}})])
+    (let [favorites (favorites/list-favorites ticket user)]
+      ;; count favorites
+      (is (> (get-in favorites [:body :list :pagination :count]) 0))
+      ;; check if node belongs to favorites
+      (is (some true? (map #(= (get-in % [:entry :target-guid]) node-id) (get-in favorites [:body :list :entries])))))
+    ;; clean up
+    (is (= 204 (:status (nodes/delete-node ticket node-id))))))
