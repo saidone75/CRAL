0da3e6917893ef5a4b3fbe9382d509302a29bca4
Marco Marini <mmarini@tai.it>
2024-03-29 12:18:59 +0100

    test for test-utils

---
+++ test/cral/test_utils_test.clj
@@ -0,0 +1,13 @@
+(ns cral.test-utils-test
+  (:require [clojure.test :refer :all]
+            [cral.alfresco.auth :as auth]
+            [cral.alfresco.config :as config]
+            [cral.test-utils :as test-utils]))
+
+(defonce user "admin")
+(defonce password "admin")
+
+(deftest get-guest-home
+  (config/set-log-level :trace)
+  (let [ticket (get-in (auth/create-ticket user password) [:body :entry])]
+    (is (= 200 (:status (test-utils/get-guest-home ticket))))))
