c8abb989a4d71f279d5650c7c689a757c43cc22b
saidone <saidone@saidone.org>
2024-04-04 18:40:45 +0200

    favorites test rework

--- test/cral/favorites_test.clj
+++ test/cral/favorites_test.clj
@@ -11,24 +11,49 @@
 (def user "admin")
 (def password "admin")
 
-(deftest favorites-test
+(deftest list-favorites-test
   (let [ticket (get-in (auth/create-ticket user password) [:body :entry])
-        parent-id (:id (tu/get-guest-home ticket))
         ;; create a node
-        create-node-body (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})
-        node-id (get-in (nodes/create-node ticket parent-id create-node-body) [:body :entry :id])]
+        created-node-id (get-in (nodes/create-node ticket (:id (tu/get-guest-home ticket)) (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])]
     ;; add node to favorites
-    (let [favorite-id (get-in (favorites/create-favorite ticket "-me-" [(model/->CreateFavoriteBody {:file {:guid node-id}})]) [:body :entry :target-guid])]
-      ;; get favorite
-      (is (= (:status (favorites/get-favorite ticket "-me-" favorite-id)) 200))
-      ;; list favorites
-      (let [list-favorites-response (favorites/list-favorites ticket user)]
-        (is (= (:status list-favorites-response) 200))
-        ;; count favorites
-        (is (> (get-in list-favorites-response [:body :list :pagination :count]) 0))
-        ;; check if node belongs to favorites
-        (is (some true? (map #(= (get-in % [:entry :target-guid]) node-id) (get-in list-favorites-response [:body :list :entries])))))
-      ;; delete favorite
-      (is (= (:status (favorites/delete-favorite ticket "-me-" favorite-id)) 204)))
+    (favorites/create-favorite ticket "-me-" [(model/->CreateFavoriteBody {:file {:guid created-node-id}})])
+    ;; list favorites
+    (let [list-favorites-response (favorites/list-favorites ticket user)]
+      (is (= (:status list-favorites-response) 200))
+      ;; count favorites
+      (is (> (get-in list-favorites-response [:body :list :pagination :count]) 0))
+      ;; check if node belongs to favorites
+      (is (some true? (map #(= (get-in % [:entry :target-guid]) created-node-id) (get-in list-favorites-response [:body :list :entries])))))
     ;; clean up
-    (is (= (:status (nodes/delete-node ticket node-id)) 204))))
+    (is (= (:status (nodes/delete-node ticket created-node-id)) 204))))
+
+(deftest create-favorite-test
+  (let [ticket (get-in (auth/create-ticket user password) [:body :entry])
+        ;; create a node
+        created-node-id (get-in (nodes/create-node ticket (:id (tu/get-guest-home ticket)) (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])]
+    ;; create favorite
+    (is (= (:status (favorites/create-favorite ticket "-me-" [(model/->CreateFavoriteBody {:file {:guid created-node-id}})])) 201))
+    ;; clean up
+    (is (= (:status (nodes/delete-node ticket created-node-id)) 204))))
+
+(deftest get-favorite-test
+  (let [ticket (get-in (auth/create-ticket user password) [:body :entry])
+        ;; create a node
+        created-node-id (get-in (nodes/create-node ticket (:id (tu/get-guest-home ticket)) (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])
+        ;; add node to favorites
+        created-favorite-id (get-in (favorites/create-favorite ticket "-me-" [(model/->CreateFavoriteBody {:file {:guid created-node-id}})]) [:body :entry :target-guid])]
+    ;; get favorite
+    (is (= (:status (favorites/get-favorite ticket "-me-" created-favorite-id)) 200))
+    ;; clean up
+    (is (= (:status (nodes/delete-node ticket created-node-id)) 204))))
+
+(deftest delete-favorite-test
+  (let [ticket (get-in (auth/create-ticket user password) [:body :entry])
+        ;; create a node
+        created-node-id (get-in (nodes/create-node ticket (:id (tu/get-guest-home ticket)) (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type "cm:content"})) [:body :entry :id])
+        ;; add node to favorites
+        created-favorite-id (get-in (favorites/create-favorite ticket "-me-" [(model/->CreateFavoriteBody {:file {:guid created-node-id}})]) [:body :entry :target-guid])]
+    ;; delete favorite
+    (is (= (:status (favorites/delete-favorite ticket "-me-" created-favorite-id)) 204))
+    ;; clean up
+    (is (= (:status (nodes/delete-node ticket created-node-id)) 204))))
