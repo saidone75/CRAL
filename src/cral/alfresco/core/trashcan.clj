3ac96e606e8738ca52f6d729cdd7313e44761e74
saidone <saidone@saidone.org>
2024-04-04 08:15:53 +0200

    restore-deleted-nodes

--- src/cral/alfresco/core/trashcan.clj
+++ src/cral/alfresco/core/trashcan.clj
@@ -1,12 +1,13 @@
 (ns cral.alfresco.core.trashcan
   (:require [clj-http.lite.client :as client]
+            [clojure.data.json :as json]
             [cral.alfresco.config :as config]
             [cral.alfresco.model.auth]
             [cral.alfresco.model.core]
             [cral.utils.utils :as utils])
   (:import (clojure.lang PersistentHashMap)
            (cral.alfresco.model.auth Ticket)
-           (cral.alfresco.model.core GetDeletedNodeContentQueryParams GetDeletedNodeQueryParams ListDeletedNodesQueryParams)))
+           (cral.alfresco.model.core GetDeletedNodeContentQueryParams GetDeletedNodeQueryParams ListDeletedNodesQueryParams RestoreDeletedNodeBody RestoreDeletedNodeQueryParams)))
 
 (defn list-deleted-nodes
   "Gets a list of deleted nodes for the current user.
@@ -58,4 +59,19 @@
      (format "%s/deleted-nodes/%s/content" (config/get-url 'core) node-id)
      ticket
      {:query-params query-params :as :byte-array}
+     opts)))
+
+(defn restore-deleted-node
+  "Attempts to restore the deleted node **node-id** to its original location or to a new location.
+  More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/trashcan/restoreDeletedNode)."
+  ([^Ticket ticket ^String node-id ^RestoreDeletedNodeBody body]
+   (restore-deleted-node ticket node-id body nil))
+  ([^Ticket ticket ^String node-id ^RestoreDeletedNodeBody body ^RestoreDeletedNodeQueryParams query-params & [^PersistentHashMap opts]]
+   (utils/call-rest
+     client/post
+     (format "%s/deleted-nodes/%s/restore" (config/get-url 'core) node-id)
+     ticket
+     {:body         (json/write-str (utils/camel-case-stringify-keys body))
+      :query-params query-params
+      :content-type :json}
      opts)))
