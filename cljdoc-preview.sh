430cf1e5278a8b009c4c92cd62fb9f609393cba9
saidone <saidone@saidone.org>
2024-04-14 09:51:34 +0200

    add cljdoc-preview.sh

---
+++ cljdoc-preview.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# get project name
+PROJECT=$(cat project.clj | grep defproject | cut -d " " -f 2 | cut -d "/" -f 2)
+# get group id
+GROUP_ID=$(cat project.clj | grep defproject | cut -d " " -f 2 | cut -d "/" -f 1)
+# get version
+VERSION=$(cat project.clj | grep defproject | cut -d " " -f 3 | tr -d '"')
+
+echo "Generating cljdoc for $PROJECT-$VERSION"
+
+# clean up previous run db
+sudo rm -rf /tmp/cljdoc
+mkdir -p /tmp/cljdoc
+
+# build and install into local repo
+echo "Installing $PROJECT-$VERSION jar and pom into local repo"
+lein install
+
+# ingest into cljdoc
+docker run --rm \
+  -v $(pwd):/$PROJECT \
+  -v "$HOME/.m2:/root/.m2" \
+  -v /tmp/cljdoc:/app/data \
+  --entrypoint clojure \
+  cljdoc/cljdoc -Sforce -M:cli ingest \
+    --project $GROUP_ID/$PROJECT \
+    --version $VERSION \
+    --git /$PROJECT \
+
+# start server
+docker run --rm -p 8000:8000 -v /tmp/cljdoc:/app/data -v "$HOME/.m2:/root/.m2" cljdoc/cljdoc
