1f3fedc1ef81354a7336f33121bf22ae0c0151aa
saidone <saidone@saidone.org>
2024-04-14 10:13:09 +0200

    fix cljdoc in favorites and groups

--- src/cral/alfresco/core/groups.clj
+++ src/cral/alfresco/core/groups.clj
@@ -20,10 +20,9 @@
                                      UpdateGroupQueryParams)))
 
 (defn list-user-group-memberships
-  "Gets a list of group membership information for person **person-id**.
-  You can use the `-me-` string in place of `person-id` to specify the currently authenticated user.
-  You can use the **include** parameter to return additional information.
-  You can use the **where** parameter to filter the returned groups by **is-root**.
+  "Gets a list of group membership information for person `person-id`.
+  You can use the **-me-** string in place of `person-id` to specify the currently authenticated user.
+  You can use the **include** parameter in `query-params` to return additional information.
   More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/groups/listGroupMembershipsForPerson)."
   ([^Ticket ticket ^String person-id]
    (list-user-group-memberships ticket person-id nil))
@@ -37,8 +36,7 @@
 
 (defn list-groups
   "Gets a list of groups.
-  You can use the **include** parameter to return additional information.
-  You can use the **where** parameter to filter the returned groups by **is-root**.
+  You can use the **include** parameter in `query-params` to return additional information.
   More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/groups/listGroups)."
   ([^Ticket ticket]
    (list-groups ticket nil))
@@ -54,7 +52,7 @@
   "Create a group.
   The group id must start with \"GROUP_\". If this is omitted it will be added automatically.
   This format is also returned when listing groups or group memberships. It should be noted that the other group-related operations also expect the id to start with \"GROUP_\".
-  If one or more **parent-ids** are specified then the group will be created and become a membe of each of the specified parent groups.
+  If one or more **parent-ids** are specified then the group will be created and become a member of each of the specified parent groups.
   If no **parent-ids** are specified then the group will be created as a root group.
   The group will be created in the **APP.DEFAULT** and **AUTH.ALF** zones.
   You must have admin rights to create a group.
@@ -72,8 +70,8 @@
      opts)))
 
 (defn get-group-details
-  "Get details for **group group-id**.
-  You can use the **include** parameter to return additional information.
+  "Get details for group `group-id`.
+  You can use the **include** parameter in `query-params` to return additional information.
   More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/groups/getGroup)."
   ([^Ticket ticket ^String group-id]
    (get-group-details ticket group-id nil))
@@ -86,7 +84,7 @@
      opts)))
 
 (defn update-group-details
-  "Update details (display-name) for group **group-id**.
+  "Update details (display-name) for group `group-id`.
   You must have admin rights to update a group.
   More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/groups/updateGroup)."
   ([^Ticket ticket ^String group-id ^UpdateGroupBody body]
@@ -102,7 +100,7 @@
      opts)))
 
 (defn delete-group
-  "Delete group **group-id**.
+  "Delete group `group-id`.
   The option to cascade delete applies this recursively to any hierarchy of group members.
   In this case, removing a group member does not delete the person or sub-group itself.
   If a removed sub-group no longer has any parent groups then it becomes a root group.
@@ -119,7 +117,7 @@
      opts)))
 
 (defn create-group-membership
-  "Create a group membership (for an existing person or group) within a group **group-id**.
+  "Create a group membership (for an existing person or group) within a group `group-id`.
   If the added group was previously a root group then it becomes a non-root group since it now has a parent.
   It is an error to specify an id that does not exist.
   You must have admin rights to create a group membership.
@@ -137,8 +135,8 @@
      opts)))
 
 (defn list-group-memberships
-  "Gets a list of the group memberships for the group **group-id**.
-  You can use the where parameter to filter the returned groups by **member-type**.
+  "Gets a list of the group memberships for the group `group-id`.
+  You can use the where parameter in `query-params` to filter the returned groups by **member-type**.
   More info [here](https://api-explorer.alfresco.com/api-explorer/?urls.primaryName=Core%20API#/groups/listGroupMemberships)."
   ([^Ticket ticket ^String group-id]
    (list-group-memberships ticket group-id nil))
@@ -151,7 +149,7 @@
      opts)))
 
 (defn delete-group-membership
-  "Delete group member **group-member-id** (person or sub-group) from group **group-id**.
+  "Delete group member `group-member-id` (person or sub-group) from group `group-id`.
   Removing a group member does not delete the person or sub-group itself.
   If a removed sub-group no longer has any parent groups then it becomes a root group.
   You must have admin rights to delete a group membership.
