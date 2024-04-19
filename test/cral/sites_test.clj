994d9286e3f610e54341472f8171c65f66960d1c
Marco Marini <mmarini@tai.it>
2024-04-19 17:57:08 +0200

    delete-site-test

--- test/cral/sites_test.clj
+++ test/cral/sites_test.clj
@@ -279,30 +279,14 @@
     ;; clean up
     (is (= (:status (sites/delete-site ticket site-id (model/map->DeleteSiteQueryParams {:permanent true}))) 204))))
 
-;; old tests
-(deftest create-then-list-then-update-then-get-then-delete-site
+(deftest delete-site-test
   (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
         site-id (.toString (UUID/randomUUID))
-        create-site-body (model/map->CreateSiteBody {:title site-id :id site-id :visibility "PUBLIC"})
-        ;; create site
-        create-site-response (sites/create-site ticket create-site-body)]
-    (is (= (:status create-site-response) 201))
-    ;; list-sites
-    (let [list-sites-response (sites/list-sites ticket)]
-      (is (= (:status list-sites-response) 200))
-      ;; check if site is present
-      (is (some true?
-                (map
-                  #(= site-id (get-in % [:entry :id]))
-                  (get-in list-sites-response [:body :list :entries])))))
-    ;; update site
-    (let [update-site-body (model/map->UpdateSiteBody {:title (.toString (UUID/randomUUID)) :visibility "PRIVATE"})
-          update-site-response (sites/update-site ticket site-id update-site-body)]
-      (is (= (:status update-site-response) 200))
-      ;; check if title and visibility have been updated
-      (is (= (get-in update-site-response [:body :entry :title]) (:title update-site-body)))
-      (is (= (get-in update-site-response [:body :entry :visibility]) (:visibility update-site-body))))
-    ;; get-site
+        ;; create a public site
+        _ (->> (model/map->CreateSiteBody {:title site-id :id site-id :visibility "PUBLIC"})
+               (sites/create-site ticket))]
     (is (= (:status (sites/get-site ticket site-id)) 200))
     ;; delete site
-    (is (= (:status (sites/delete-site ticket site-id (model/map->DeleteSiteQueryParams {:permanent true}))) 204))))
+    (is (= (:status (sites/delete-site ticket site-id (model/map->DeleteSiteQueryParams {:permanent true}))) 204))
+    ;; check if site has been deleted
+    (is (= (:status (sites/get-site ticket site-id)) 404))))
