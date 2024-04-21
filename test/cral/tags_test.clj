eb21bddf6b1432820abefe842f31455225870fde
saidone <saidone@saidone.org>
2024-04-21 11:43:43 +0200

    delete-tag-test

--- test/cral/tags_test.clj
+++ test/cral/tags_test.clj
@@ -124,29 +124,14 @@
     ;; clean up
     (is (= (:status (tags/delete-tag ticket created-tag-id)) 204))))
 
-;; old tests
-(deftest create-then-list-then-get-then-delete-node-tags
+(deftest delete-tag-test
   (let [ticket (get-in (auth/create-ticket user pass) [:body :entry])
-        parent-id (tu/get-guest-home ticket)
-        ;; create a node
-        node-id (get-in (nodes/create-node ticket parent-id (model/map->CreateNodeBody {:name (.toString (UUID/randomUUID)) :node-type cm/type-content})) [:body :entry :id])
-        tag-name (.toString (UUID/randomUUID))]
-    ;; create a tag for the node
-    (= (:status (tags/create-node-tag ticket node-id [(model/map->CreateNodeTagBody {:tag tag-name})])) 201)
-    ;; list tags
-    (let [list-node-tag-response (tags/list-node-tags ticket node-id)]
-      (is (= (:status list-node-tag-response) 200))
-      (is (= (get-in (first (get-in list-node-tag-response [:body :list :entries])) [:entry :tag]) tag-name))
-      ;; get tag
-      (is (= (:status (tags/get-tag ticket (get-in (first (get-in list-node-tag-response [:body :list :entries])) [:entry :id]))) 200))
-      ;; update tag
-      (let [new-tag-name (.toString (UUID/randomUUID))
-            update-tag-response (tags/update-tag ticket (get-in (first (get-in list-node-tag-response [:body :list :entries])) [:entry :id]) (model/map->UpdateTagBody {:tag new-tag-name}))]
-        (is (= (:status update-tag-response) 200))
-        (is (= (get-in update-tag-response [:body :entry :tag]) new-tag-name)))
-      ;; delete tag
-      (is (= (:status (tags/delete-node-tag ticket node-id (get-in (first (get-in list-node-tag-response [:body :list :entries])) [:entry :id]))) 204)))
+        ;; create a tag
+        created-tag-id (get-in (->> (model/map->CreateTagBody {:tag (.toString (UUID/randomUUID))})
+                                    (tags/create-tag ticket)) [:body :entry :id])]
+    ;; delete tag
+    (is (= (:status (tags/delete-tag ticket created-tag-id)) 204))
     ;; check if tag has been deleted
-    (is (empty? (get-in (tags/list-node-tags ticket node-id) [:body :list :entries])))
+    (is (not (some #(= (get-in % [:entry :id]) created-tag-id) (get-in (tags/list-tags ticket) [:body :list :entries]))))
     ;; clean up
-    (is (= (:status (nodes/delete-node ticket node-id)) 204))))
+    (is (= (:status (tags/delete-tag ticket created-tag-id)) 404))))
