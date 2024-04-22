e9de2dfe1a99a0ac41264b423cc0590863649f58
Saidone <saidone@saidone.org>
2024-04-22 12:24:12 +0200

    Update cm.clj

--- src/cral/model/alfresco/cm.clj
+++ src/cral/model/alfresco/cm.clj
@@ -14,7 +14,7 @@
 ;  You should have received a copy of the GNU General Public License
 ;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-; generated with https://saidone.org/#/cm
+; generated with https://saidone.org/#/cm-clj
 
 (ns cral.model.alfresco.cm)
 
