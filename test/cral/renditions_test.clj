2f0eba11df6f4862e9574d516e574a49597f8fdd
saidone <saidone@saidone.org>
2024-06-08 18:51:25 +0200

    create-rendition-test

---
+++ test/cral/renditions_test.clj
@@ -0,0 +1,43 @@
+;  CRAL
+;  Copyright (C) 2023-2024 Saidone
+;
+;  This program is free software: you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, either version 3 of the License, or
+;  (at your option) any later version.
+;
+;  This program is distributed in the hope that it will be useful,
+;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;  GNU General Public License for more details.
+;
+;  You should have received a copy of the GNU General Public License
+;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(ns cral.renditions-test
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [cral.api.auth :as auth]
+            [cral.api.core.nodes :as nodes]
+            [cral.api.core.renditions :as renditions]
+            [cral.config :as c]
+            [cral.fixtures :as fixtures]
+            [cral.model.alfresco.cm :as cm]
+            [cral.model.core :as model]
+            [cral.test-utils :as tu])
+  (:import (java.util UUID)))
+
+(use-fixtures :once fixtures/setup)
+
+(deftest create-rendition-test
+  (let [ticket (get-in (auth/create-ticket c/user c/password) [:body :entry])
+        ;; create a node
+        created-node-id (->> (model/map->CreateNodeBody {:name (str (.toString (UUID/randomUUID)) ".jpg") :node-type cm/type-content})
+                             (nodes/create-node ticket (tu/get-guest-home ticket))
+                             (#(get-in % [:body :entry :id])))
+        ;; update the node content
+        _ (nodes/update-node-content ticket created-node-id (io/as-file (io/resource "Elkjaer_Briegel.jpg")))
+        create-rendition-response (renditions/create-rendition ticket created-node-id [(model/map->CreateRenditionBody {:id "doclib"})])]
+    (is (= (:status create-rendition-response) 202))
+    ;; clean up
+    (is (= (:status (nodes/delete-node ticket created-node-id {:permanent true})) 204))))
