3db4a2a6ce294b20a304ffe2b148609d658a24a6
saidone <saidone@saidone.org>
2024-07-03 07:32:17 +0200

    bump version

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
 ;  You should have received a copy of the GNU General Public License
 ;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(defproject org.saidone/cral "0.3.5-SNAPSHOT"
+(defproject org.saidone/cral "0.3.5"
   :description "A library for consuming Alfresco Content Services public REST API"
   :url "https://saidone.org"
   :license {:name "GNU General Public License v3.0"
